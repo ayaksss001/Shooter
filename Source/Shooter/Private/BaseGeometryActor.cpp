6d6b1f0a59240c49adf3b38aa943b1fba3994afd
Ayaksss001 <ayaksss.001@gmail.com>
2025-07-09 20:38:57 +0300

    Log Category

--- Source/Shooter/Private/BaseGeometryActor.cpp
+++ Source/Shooter/Private/BaseGeometryActor.cpp
@@ -1,6 +1,8 @@
 #include "BaseGeometryActor.h"
 
 
+DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
+
 ABaseGeometryActor::ABaseGeometryActor()
 {
  	
@@ -12,7 +14,8 @@ ABaseGeometryActor::ABaseGeometryActor()
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
-
+	UE_LOG(LogBaseGeometry, Display, TEXT("From LogBaseGeometry"))
+	
 	
 }
 
@@ -21,5 +24,19 @@ void ABaseGeometryActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void ABaseGeometryActor::printTypes()
+{
+	int WeaponNums = 4;
+	int KillsNum = 8;
+	float Health = 89.23f
+	bool IsDead = false;
+	bool HasWeapon = true;
+
+	UE_LOG(LogTemp, Display, TEXT("Weapons num: %d , Kills num: %d"), WeaponNums, KillsNum);
+	UE_LOG(LogTemp, Display, TEXT("Health: %f"), Health);
+	UE_LOG(LogTemp, Display, TEXT("Is dead: %d"), IsDead);
+	UE_LOG(LogTemp, Display, TEXT("Has Weapon: %d"), static_cast<int>(HasWeapon));
 }
 
