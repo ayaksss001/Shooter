d908063534551f90dca939fff74d1659401378c7
Ayaksss001 <ayaksss.001@gmail.com>
2025-07-13 14:37:23 +0300

    Update

--- Source/Shooter/Private/BaseGeometryActor.cpp
+++ Source/Shooter/Private/BaseGeometryActor.cpp
@@ -1,5 +1,5 @@
 #include "BaseGeometryActor.h"
-
+#include "Engine/Engine.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogBaseGeometry, All, All)
 
@@ -14,9 +14,23 @@ ABaseGeometryActor::ABaseGeometryActor()
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogBaseGeometry, Display, TEXT("From LogBaseGeometry"))
-		printTypes();
-	
+
+	FString Name = "Kirill Dyakov";
+
+	int KillsNum = 8;
+	float Health = 89.23f;
+	bool IsDead = false;
+
+	FString KillsStr = "Kills : " + FString::FromInt(KillsNum);
+	FString HealthStr = "Health : " + FString::SanitizeFloat(Health);
+	FString IsDeadStr = "Is dead : " + FString(IsDead ? "true" : "false");
+
+	FString Stats = FString::Printf(TEXT("\n ***All Stats***\n %s \n %s\n %s\n", *KillsStr, *HealthStr, *IsDeadStr));
+	UE_LOG(LogBaseGeomety, Warning, TEXT("%s"), *Stats);
+
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name);
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Stats, true, FVector2D(1.5f, 1.5f));
+
 }
 
 // Called every frame
