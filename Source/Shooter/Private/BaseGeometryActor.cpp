76cf1ba3a4849f3fdaffe1b05fc946a15f50e04d
Ayaksss001 <ayaksss.001@gmail.com>
2025-07-13 19:27:41 +0300

    BaseMesh

--- Source/Shooter/Private/BaseGeometryActor.cpp
+++ Source/Shooter/Private/BaseGeometryActor.cpp
@@ -8,13 +8,31 @@ ABaseGeometryActor::ABaseGeometryActor()
  	
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>("BaseMesh");
+	SetRootComponent(BaseMesh);
 }
 
 // Called when the game starts or when spawned
 void ABaseGeometryActor::BeginPlay()
 {
 	Super::BeginPlay();
+	printTransform();
+	InitialLocation = GetActorLocation();
+	
+}
 
+// Called every frame
+void ABaseGeometryActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector CurrentLocation = GetActorLocation();
+	float time = GetWorld()->GetTimeSeconds();
+	CurrentLocatin.Z = InitialLocation.Z + Amplitude * FMath::Sin(Friquency * time);
+	SetActorLocation(CurrentLocation);
+}
+
+void ABaseGeometryActor::printTypes()
+{
 	FString Name = "Kirill Dyakov";
 
 	int KillsNum = 8;
@@ -31,26 +49,25 @@ void ABaseGeometryActor::BeginPlay()
 	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, Name);
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Stats, true, FVector2D(1.5f, 1.5f));
 
-}
-
-// Called every frame
-void ABaseGeometryActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-void ABaseGeometryActor::printTypes()
-{
-	int WeaponNums = 4;
-	int KillsNum = 8;
-	float Health = 89.23f;
-	bool IsDead = false;
-	bool HasWeapon = true;
 
 	UE_LOG(LogTemp, Display, TEXT("Weapons num: %d , Kills num: %d"), WeaponNums, KillsNum);
 	UE_LOG(LogTemp, Display, TEXT("Health: %f"), Health);
 	UE_LOG(LogTemp, Display, TEXT("Is dead: %d"), IsDead);
 	UE_LOG(LogTemp, Display, TEXT("Has Weapon: %d"), static_cast<int>(HasWeapon));
+}
+
+void ABaseGeometryActor::printTransform()
+{
+	FTransform  Transform = GetActorTransform();
+	FVector Location = Transform.GetLocation();
+	FRotator Rotation = Transform.Rotator();
+	FVector Scale = Transform.GetScale3D();
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Transform %s"), Transform.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Location %s"), Location.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Rotation %s"), Rotation.ToString());
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Scale %s"), Scale.ToString());
+
+	UE_LOG(LogBaseGeometry, Warning, TEXT("Human transform %s"), *Transform.ToHumanReadebleStrig());
 }
 
--- Source/Shooter/Public/BaseGeometryActor.h
+++ Source/Shooter/Public/BaseGeometryActor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
 
 #include "BaseGeometryActor.generated.h"
 
@@ -16,15 +17,42 @@ public:
 	// Sets default values for this actor's properties
 	ABaseGeometryActor();
 
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Amplitude = 50.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Frequency = 2.0f;
+
+	UPROPERTY(EditAnywhere)
+	int WeaponNums = 4;
+
+	UPROPERTY(EditAnywhere)
+	int KillsNum = 8;
+
+	UPROPERTY(EditAnywhere)
+	float Health = 89.23f;
+
+	UPROPERTY(EditAnywhere)
+	bool IsDead = false;
+
+	UPROPERTY(EditAnywhere)
+	bool HasWeapon = true;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 private:
+	FVector InitialLocation;
+	
 	void printTypes();
+	void printTransform();
 
 };
